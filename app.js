80bc324fbc42922faf9341d6ffcaaab9131df5a1
Ravikiran <ravikiran@CHOT-70884-ML1.local>
2023-12-26 20:41:14 +0530

    dayone learning

---
+++ app.js
@@ -0,0 +1,31 @@
+// var h1Element = document.createElement("h1");
+// h1Element.innerHTML = "Hello World From JS";
+
+// var rootElement  = document.getElementById('root');
+// rootElement.appendChild(h1Element);
+
+
+
+
+// const hedder = React.createElement('h1',{id: "heading"},"Hello world from react")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+const newElementForRender = React.createElement(
+    'div',
+    { id: 'parent' },
+    [
+        React.createElement('div', { id: 'chield' },
+            [
+                React.createElement('h1', {}, 'Hi im a H1 Tag'),
+                React.createElement('h2', {}, 'Hi im a H2 Tag')
+            ]),
+        React.createElement('div', { id: 'chield' },
+            [
+                React.createElement('h1', {}, 'Hi im a H1 Tag'),
+                React.createElement('h2', {}, 'Hi im a H2 Tag')
+            ])
+    ])
+
+root.render(newElementForRender);
